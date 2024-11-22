9158aae3e4055075a30df61703612a4cda1fa053
Ali <biz.alihabibi@gmail.com>
2024-11-22 20:44:16 +0330

    Implement Splash Screen

--- app/src/main/java/com/ir/kianpardaz/kianpardazlogin/MainActivity.kt
+++ app/src/main/java/com/ir/kianpardaz/kianpardazlogin/MainActivity.kt
@@ -2,12 +2,16 @@ package com.ir.kianpardaz.kianpardazlogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.window.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.ir.kianpardaz.kianpardazlogin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(950)
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
